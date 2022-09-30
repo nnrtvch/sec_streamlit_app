0cf22b5d16e2229daec7117ac791d71cd25c2dfa
nnrtvch <56116379+nnrtvch@users.noreply.github.com>
2022-09-30 22:59:27 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ product_caption = 'Our warm, comfortable, ' + option + ' sweatsuit!'
 my_cur.execute("select direct_url, price, size_list, upsell_product_desc from catalog_for_website where color_or_style = '" + option + "'")
 df2 = my_cur.fetchone()
 
-streamlit.image(
+lit.image(
   df2[0],
   width=400,
   caption= product_caption

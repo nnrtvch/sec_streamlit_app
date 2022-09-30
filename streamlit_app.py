64fd721caa2f5f4f5a38d3ad71bf329ccee9a43a
nnrtvch <56116379+nnrtvch@users.noreply.github.com>
2022-09-30 22:58:33 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,7 @@ def get_color_list():
     return my_cur.fetchall()
 
 my_cnx = snowflake.connector.connect(**lit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
 my_data_row_2 = get_color_list()
 my_cnx.close()
 df = pd.DataFrame(my_data_row_2)
@@ -21,8 +22,8 @@ color_list = df[0].values.tolist()
 option = lit.selectbox('Pick a sweatsuit color or style:', list(color_list))
 product_caption = 'Our warm, comfortable, ' + option + ' sweatsuit!'
 
-my_cur2.execute("select direct_url, price, size_list, upsell_product_desc from catalog_for_website where color_or_style = '" + option + "'")
-df2 = my_cur2.fetchone()
+my_cur.execute("select direct_url, price, size_list, upsell_product_desc from catalog_for_website where color_or_style = '" + option + "'")
+df2 = my_cur.fetchone()
 
 streamlit.image(
   df2[0],

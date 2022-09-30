b67437dec76c7fb0c077b79ab3f543854bc44e01
nnrtvch <56116379+nnrtvch@users.noreply.github.com>
2022-09-30 22:41:48 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,9 +15,18 @@ if lit.button('Get fruit list'):
   my_cnx = snowflake.connector.connect(**lit.secrets["snowflake"])
   my_data_row = get_color_list()
   my_cnx.close()
-  lit.dataframe(my_data_row)
-
+  ert = list(my_data_row)
+  lit.dataframe(ert)
 lit.stop()
+  
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+fruits_selected  = lit.multiselect('Pick some fruits: ', list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+lit.dataframe(fruits_to_show)
+
+
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')

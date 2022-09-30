b6cd0c11b946dd771d8d5603aa8d980dc4a37966
nnrtvch <56116379+nnrtvch@users.noreply.github.com>
2022-09-30 23:07:33 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import requests
 import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**lit.secrets["snowflake"]
-mu_cur = my_cnx.cursor()
+my_cur = my_cnx.cursor()
                                    
 my_cur.execute("select color_or_style from catalog_for_website")
 color_catalogue = my_cur.fetch_all()
